f1bc5ab25693bbf3d1801075b5b003959d6e8ff0
DNS223 <npm030312@gmail.com>
2024-06-06 14:38:18 -0600

    "Input Search"

--- src/components/sideBar/sideBar.jsx
+++ src/components/sideBar/sideBar.jsx
@@ -6,15 +6,17 @@ import { GrDocumentText } from "react-icons/gr";
 import { GiCommercialAirplane } from "react-icons/gi";
 import CelebrationIcon from '@mui/icons-material/Celebration';
 import { FaRegClock } from "react-icons/fa";
-
+import { FaGears } from "react-icons/fa6";
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 
 
 export default function SideBar (){
     return(
         <div className={styles.container}>
             <Image src="/Logo-vasa.png" width={80} height={30}/>
+            <input type="text" placeholder='Buscar...'/>
             <div className={styles.links1}>
-            <Link href=""> <span><IoHome/></span> Inicio</Link>
+            <Link href="/"> <span><IoHome/></span> Inicio</Link>
             <Link href=""> <span><GrDocumentText/></span>  Expediente Empleados</Link>
             <Link href=""> <span><GiCommercialAirplane/></span> Vacaciones</Link>
             <Link href=""> <span><CelebrationIcon/></span> Cumpleaños</Link>
@@ -22,8 +24,8 @@ export default function SideBar (){
             </div>
            
            <div className={styles.links2}>
-           <Link href=""> <span><IoHome/></span> Inicio</Link>
-            <Link href=""> <span><GrDocumentText/></span>  Expediente Empleados</Link>
+           <Link href=""> <span><FaGears/></span> Configuración</Link>
+            <Link href=""> <span><AccountCircleOutlinedIcon/></span>  Perfil</Link>
 
            </div>
         
